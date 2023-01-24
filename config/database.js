b110af060572a677432ee5780ef363626c431ac5
Bregsi Atingsari Julastri <20081010211@student.upnjatim.ac.id>
2023-01-24 22:20:38 +0700

    fix deploy on heroku

--- config/database.js
+++ config/database.js
@@ -27,6 +27,11 @@ const db = {
     host: DB_HOST,
     port: DB_PORT,
     dialect: 'postgres',
+    dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
   },
 };
 module.exports = db;
